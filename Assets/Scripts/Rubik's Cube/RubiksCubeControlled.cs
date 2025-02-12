743c06fa7ab1cbcc008d49623c7b3f2c48a90ef6
Sacha <S.Soulslasher@gmail.com>
2025-02-12 12:59:12 +0100

    [ADD] Control Script

---
+++ Assets/Scripts/Rubik's Cube/RubiksCubeControlled.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubiksCubeControlled : MonoBehaviour
+{
+
+    GameObject Lilcube;
+
+    void Update()
+    {
+        
+    }
+
+
+
+
+}
