44d7b12b740fd6baf1efba929003e3d5133c8766
ShaneDorin <shane.dorin@edu.devinci.fr>
2025-02-14 10:03:48 +0100

    [REMOVE]  Raycast child-object, [EDIT] MouseCamControl Script
    Moved Raycast code from Raycast script to the MouseCamControl Script.

--- Assets/Game/FPS-Player/MouseCamControl.cs
+++ Assets/Game/FPS-Player/MouseCamControl.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class MouseCamControl : MonoBehaviour
 {
-
-    [SerializeField] float _mouseCamControlSpeed = 100f;
+    [Header("Camera Movement")]
     [SerializeField] Transform _playerTransform;
+    [SerializeField] float _mouseCamControlSpeed = 100f;
+
+
+    [Header ("Raycast")]
+    [SerializeField] RubiksCubeController rubiksCubeController;
+    [SerializeField] LayerMask _detectableLayer;
+    [SerializeField] float _maxDistance;
+
 
     float _xRotation;
+
+    RaycastHit _raycastInfo;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +27,7 @@ public class MouseCamControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Camera movement
         float mouseX = Input.GetAxis("Mouse X") * _mouseCamControlSpeed * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * _mouseCamControlSpeed * Time.deltaTime;
 
@@ -26,5 +36,13 @@ public class MouseCamControl : MonoBehaviour
         transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
 
         _playerTransform.Rotate(Vector3.up * mouseX);
+
+
+        //Raycast
+        if (Physics.Raycast(transform.position, transform.forward, out _raycastInfo, _maxDistance, _detectableLayer))
+        {
+            Debug.Log(_raycastInfo.transform.name);
+            rubiksCubeController.SetActualFace(_raycastInfo.transform.gameObject);
+        }
     }
 }
