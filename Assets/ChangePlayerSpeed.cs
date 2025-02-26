d5eb872a474659136c965f4491a8e447005f0eea
ShaneDorin <shane.dorin@edu.devinci.fr>
2025-02-26 15:38:27 +0100

    [ADD] SlipperyZone prefab

--- Assets/ChangePlayerSpeed.cs
+++ Assets/ChangePlayerSpeed.cs
@@ -6,24 +6,13 @@ public class ChangePlayerSpeed : MonoBehaviour
 {
 
     [SerializeField] float newSpeedMultiplyer = 0.5f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Entered");
         if (other.gameObject.tag != "Player") return;
         PlayerMovement playerMovement =  other.gameObject.GetComponent<PlayerMovement>();
-        playerMovement.setSpeed(playerMovement.defaultSpeed *  newSpeedMultiplyer);
+        playerMovement.SetSpeed(playerMovement.defaultSpeed *  newSpeedMultiplyer);
     }
 
     private void OnTriggerExit(Collider other)
@@ -31,6 +20,6 @@ public class ChangePlayerSpeed : MonoBehaviour
         Debug.Log("Exited");
         if (other.gameObject.tag != "Player") return;
         PlayerMovement playerMovement = other.gameObject.GetComponent<PlayerMovement>();
-        playerMovement.setSpeedToDefault();
+        playerMovement.SetSpeedToDefault();
     }
 }
