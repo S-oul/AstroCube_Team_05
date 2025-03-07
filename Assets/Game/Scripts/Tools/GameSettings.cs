d35f212c88d739156fd3196164ee793be807da21
BBouleDeSuiff <emaouardi@gmail.com>
2025-03-07 11:55:44 +0100

    [ADD] Headbobbing

--- Assets/Game/Scripts/Tools/GameSettings.cs
+++ Assets/Game/Scripts/Tools/GameSettings.cs
@@ -15,6 +15,11 @@ public class GameSettings : ScriptableObject
     public float CrouchSpeed => _crouchSpeed;
     public float CrouchHeight => _crouchHeight;
     public float SlippingMovementControl => _slippingMovementControl;
+    public float HeadBobbingAmount => _headBobbingAmount;
+    public float HeadBobbingSpeed => _headBobbingSpeed;
+    public float StartWalkingTransitionDuration => _startWalkingTransitionDuration;
+    public float StopWalkingTransitionDuration => _stopWalkingTransitionDuration;
+    public AnimationCurve HeadBobbingCurve => _headBobbingCurve;
     public bool EnableGravityRotation => _enableGravityRotation;
 
 
@@ -45,9 +50,15 @@ public class GameSettings : ScriptableObject
     [Header("- Slipping")]
     [SerializeField][Range(0.0f, 0.1f)] float _slippingMovementControl = 0.01f;
 
+    [Header("- HeadBobbing")]
+    [SerializeField] float _headBobbingAmount = 2.0f;
+    [SerializeField] float _headBobbingSpeed = 3.0f;
+    [SerializeField] float _startWalkingTransitionDuration = 0.5f;
+    [SerializeField] float _stopWalkingTransitionDuration = 0.5f;
+    [SerializeField] AnimationCurve _headBobbingCurve;
+
     [Header("- GravityRotation")]
     [SerializeField] bool _enableGravityRotation = false;
-
 
     [Header("-- RUBIK'S CUBE --")]
     [SerializeField] private float _rubikscCubeAxisRotationDuration = 0.2f;
