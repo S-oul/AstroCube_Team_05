c7046bc6deb07b4c00a235878386600a27443628
leopoldlecl <115979417+LeopoldLecl@users.noreply.github.com>
2025-03-07 12:08:24 +0100

    [ADD] visual when link betwen electric points
    bug doesnt work when collider rotate fix : augment size collider

--- Assets/Game/Scripts/LD_Elements/Constelation/ElectricCircuitScript.cs
+++ Assets/Game/Scripts/LD_Elements/Constelation/ElectricCircuitScript.cs
@@ -1,14 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ElectricCircuitScript : MonoBehaviour
 {
     [SerializeField] private GameObject previousElectricPoint;
-    [SerializeField] private GameObject nextElectricPoint;
-
     [SerializeField] private GameObject Editor_InputVisual;
     [SerializeField] private GameObject Editor_OutputVisual;
+
+    [SerializeField] private float lineStartWidth = 0.05f;  // Largeur de début du faisceau
+    [SerializeField] private float lineEndWidth = 0.05f;    // Largeur de fin du faisceau
+
+    private LineRenderer lineRenderer;
+    private ParticleSystem electricParticles;
 
     private void Awake()
     {
@@ -20,28 +23,75 @@ public class ElectricCircuitScript : MonoBehaviour
         {
             Editor_OutputVisual.SetActive(false);
         }
-    }
 
+        // Ajouter un LineRenderer pour dessiner la connexion
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.startWidth = lineStartWidth;
+        lineRenderer.endWidth = lineEndWidth;
+        lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+        lineRenderer.material.color = Color.cyan;
+        lineRenderer.enabled = false;
+
+        // Ajouter un Particle System pour les étincelles électriques
+        GameObject particleObject = new GameObject("ElectricParticles");
+        particleObject.transform.SetParent(transform);
+        electricParticles = particleObject.AddComponent<ParticleSystem>();
+
+        var main = electricParticles.main;
+        main.startColor = Color.blue;
+        main.startSpeed = 5f;
+        main.startSize = 0.1f;
+        main.loop = true;
+        main.playOnAwake = false;
+
+        var shape = electricParticles.shape;
+        shape.shapeType = ParticleSystemShapeType.Sphere;
+        shape.radius = 0.1f;
+
+        var emission = electricParticles.emission;
+        emission.rateOverTime = 50f;
+
+        var noise = electricParticles.noise;
+        noise.enabled = true;
+        noise.strength = 0.5f;
+
+        electricParticles.Stop();
+    }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("ElectricPoint"))
         {
-            Debug.Log("Collision avec un point electrique");
-            if (previousElectricPoint != null)
+            if (previousElectricPoint != null && other.gameObject == previousElectricPoint)
             {
-                if (other.gameObject == previousElectricPoint)
-                {
-                    Debug.Log("Conexion avec le previous");
-                }
+                Debug.Log("Connexion avec le previous");
+                DrawElectricBeam();
             }
-            if (nextElectricPoint != null)
-            {
-                if (other.gameObject == nextElectricPoint)
-                {
-                    Debug.Log("Conexion avec le next");
-                }
-            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ElectricPoint") && previousElectricPoint != null && other.gameObject == previousElectricPoint)
+        {
+            lineRenderer.enabled = false;
+            electricParticles.Stop();
+        }
+    }
+
+    private void DrawElectricBeam()
+    {
+        if (previousElectricPoint != null)
+        {
+            lineRenderer.enabled = true;
+            lineRenderer.startWidth = lineStartWidth;  // Mettre à jour la largeur
+            lineRenderer.endWidth = lineEndWidth;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, previousElectricPoint.transform.position);
+
+            // Activer les particules et les déplacer au milieu de la ligne
+            electricParticles.transform.position = (transform.position + previousElectricPoint.transform.position) / 2;
+            electricParticles.Play();
         }
     }
 }
