f04b9cfeb5632e0c33b961ea86ac24bb17e4cb69
leopoldlecl <115979417+LeopoldLecl@users.noreply.github.com>
2025-04-17 11:20:28 +0200

    [ADD] AudioManager setup

---
+++ Assets/Game/Scripts/Audio/AudioEventID.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AudioEventID
+{
+    // Music
+
+    // Sound Effects
+
+    // UI Sounds
+    MenuPause,
+    MenuUnpause,
+    // Miscellaneous
+
+    // Player Sounds
+
+    // Player Movement
+    PlayerJump,
+    PlayerLand,
+    Footstep_Default,
+}
