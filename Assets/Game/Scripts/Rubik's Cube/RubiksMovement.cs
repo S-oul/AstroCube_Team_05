92a316767a543c63849bd6017749996b22e0417d
BBouleDeSuiff <emaouardi@gmail.com>
2025-04-25 16:10:27 +0200

    [FIX] Blocked Player after revert + CameraClipping + block inputs during transition

--- Assets/Game/Scripts/Rubik's Cube/RubiksMovement.cs
+++ Assets/Game/Scripts/Rubik's Cube/RubiksMovement.cs
@@ -143,7 +143,10 @@ public class RubiksMovement : MonoBehaviour
     IEnumerator ReverseAllMoves(float time)
     {
         while (_isRotating) yield return null;
-        time /= _moves.Count();
+        if(_moves.Count() != 0)
+            time /= _moves.Count();
+        else
+            time = 0.0f;
         _isReversing = true;
         while (_moves.Count > 0)
         {
