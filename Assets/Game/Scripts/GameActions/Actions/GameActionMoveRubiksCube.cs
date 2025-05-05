36d39bd7fcd42cb795e34d38d56df4eb65ce693a
BBouleDeSuiff <emaouardi@gmail.com>
2025-05-05 17:26:53 +0200

    [ADD] Sequencer action Move RubicsCube Axis

---
+++ Assets/Game/Scripts/GameActions/Actions/GameActionMoveRubiksCube.cs
@@ -0,0 +1,40 @@
+using DG.Tweening;
+using NaughtyAttributes;
+using RubiksStatic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameActionMoveRubiksCube : AGameAction
+{
+
+    [SerializeField] private RubiksMove _move = new();
+    [SerializeField] private RubiksMovement _RubiksScript;
+    private bool _isTurning = false;
+
+    protected override void ExecuteSpecific()
+    {
+        StartCoroutine(DoMove());
+    }
+
+    private IEnumerator DoMove()
+    {
+        _isTurning = true;
+        yield return _RubiksScript.RotateAxisCoroutine(_move.axis, _move.cube, _move.clockWise);
+        _isTurning = false;
+    }
+
+    protected override bool IsFinishedSpecific()
+    {
+        return _isTurning;
+    }
+
+    public override string BuildGameObjectName()
+    {
+        string name = "MOVE RUBIKSCUBE ";
+        if (_move.cube != null)
+            name += $"AT AXIS {_move.axis} OF CUBE {_move.cube.name}";
+        return name;
+    }
+}
