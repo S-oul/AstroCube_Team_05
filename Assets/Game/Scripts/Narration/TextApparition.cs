0d6396c896a5acca933dd70805e11c5a1f95e439
Thomas Florent <38913690+Whismirk@users.noreply.github.com>
2025-05-13 17:23:46 +0200

    [ADD] Mergez pas encore

--- Assets/Game/Scripts/Narration/TextApparition.cs
+++ Assets/Game/Scripts/Narration/TextApparition.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -22,31 +23,57 @@ public class TextApparition : MonoBehaviour
     [field: SerializeField] public float CharFadeOutDelay { get; private set; }
     [field: SerializeField] public float CharFadeOutDuration { get; private set; }
 
+    [field: Header("Effects")]
+    [BoxGroup("Shaking"), SerializeField] private bool ShakeText;
+    [field: BoxGroup("Shaking"), ShowIf("ShakeText"), SerializeField] public float CharShakeStrength { get; private set; }
+    [field: BoxGroup("Shaking"), ShowIf("ShakeText"), SerializeField] public int CharShakeFrequency { get; private set; }
+    [field: BoxGroup("Shaking"), ShowIf("ShakeText"), SerializeField] public float CharShakeDuration { get; private set; }
+    [field: BoxGroup("Shaking"), ShowIf("ShakeText"), SerializeField] public float CharShakeRandomness { get; private set; }
+
+    private DOTweenTMPAnimator animator;
+    private Coroutine displayCo;
+
     private void Awake()
     {
         if (TryGetComponent<TMP_Text>(out _textMesh)) _textMesh.color = new Color(_textMesh.color.r, _textMesh.color.g, _textMesh.color.b, 0);
         else Debug.LogWarning("TextApparition sans texte associé sur : " + name);
+        animator = new DOTweenTMPAnimator(_textMesh);
     }
 
     
     public void Display()
     {
-        StartCoroutine(TextDisplay());
+        displayCo = StartCoroutine(TextDisplay());
     }
 
     IEnumerator TextDisplay()
     {
         yield return new WaitForSeconds(ApparitionDelay);
 
-        DOTweenTMPAnimator animator = new DOTweenTMPAnimator(_textMesh);
         for (int i = 0; i < animator.textInfo.characterCount; i++)
         {
             animator.DOFadeChar(i, 1, CharFadeInDuration);
+            if(ShakeText) animator.DOShakeCharOffset(i, CharShakeDuration, CharShakeStrength, CharShakeFrequency, CharShakeRandomness, true);
             yield return new WaitForSeconds(CharFadeInDelay);
         }
 
         yield return new WaitForSeconds(StayDuration);
+        displayCo = null;
+        Hide();
+    }
 
+    public void Hide()
+    {
+        if(displayCo != null)
+        {
+            StopCoroutine(displayCo);
+            displayCo = null;
+        }
+        StartCoroutine(TextHide());
+    }
+
+    IEnumerator TextHide()
+    {
         for (int i = 0; i < animator.textInfo.characterCount; i++)
         {
             animator.DOFadeChar(i, 0, CharFadeOutDuration);
