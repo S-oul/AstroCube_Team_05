2a354bd3b76bcaf1f7da0a56990f4018d8a6e1ff
Sacha <S.Soulslasher@gmail.com>
2025-05-13 17:15:01 +0200

    [Build] Alpha 0.7.1_Playtest

--- Assets/Game/Scripts/Narration/TextAnimation.cs
+++ Assets/Game/Scripts/Narration/TextAnimation.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.PlayerSettings;
 
 public class TextAnimation : MonoBehaviour
 {
