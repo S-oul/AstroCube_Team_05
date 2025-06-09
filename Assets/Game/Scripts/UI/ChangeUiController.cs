2972ad18bd2b6f387c26315028c78523fb1ed464
Sacha (Pc) <S.soulslasher@gmail.com>
2025-06-09 17:37:41 +0200

    [ADD] Change UI GO

--- Assets/Game/Scripts/UI/ChangeUiController.cs
+++ Assets/Game/Scripts/UI/ChangeUiController.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class ChangeUiController : MonoBehaviour
 {
-
+    [SerializeField] bool _useGameObject = false; 
+    
     [SerializeField] Sprite _Xbox;
     [SerializeField] Sprite _PS;
-    [SerializeField] Sprite _Keyboard;
+    [SerializeField, HideIf("_useGameObject")] Sprite _Keyboard;
+    [SerializeField, ShowIf("_useGameObject")] GameObject _KeyboardGO;
 
 
     Image _sprite;
@@ -32,6 +35,8 @@ public class ChangeUiController : MonoBehaviour
         InputDevice lastDevice;
         if (change == InputActionChange.ActionPerformed)
         {
+            _KeyboardGO?.SetActive(false);
+
             receivedInputAction = (InputAction)obj;
             lastDevice = receivedInputAction.activeControl.device;
 
@@ -40,7 +45,15 @@ public class ChangeUiController : MonoBehaviour
 
             if (name.Contains("XInputControllerWindows")) _sprite.sprite = _Xbox;
             else if (name.Contains("Playstation")|| name.Contains("DualSense")) _sprite.sprite = _PS;
-            else _sprite.sprite = _Keyboard;
+            else
+            {
+                if(!_useGameObject) _sprite.sprite = _Keyboard;
+                else
+                {
+                    _sprite.sprite = null;
+                    _KeyboardGO?.SetActive(true);
+                }
+            }
         }
     }
 
