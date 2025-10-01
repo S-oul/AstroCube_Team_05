79e0da65b5070edd8f86b01fb34738312fab4dac
Alienware110 <S.Soulslasher@gmail.com>
2025-10-01 11:52:57 +0200

    [WIP] Rail New physics

--- Assets/Game/Scripts/Rails/RailConnector.cs
+++ Assets/Game/Scripts/Rails/RailConnector.cs
@@ -73,6 +73,55 @@ public class RailConnector : MonoBehaviour
         return _railPointVec3[_railPointVec3.Length - 1];
     }
 
+    /// <summary>
+    /// This functions get the newPos of an object on this rail
+    /// </summary>
+    /// <param name="pos">the actualPos of the objecct on the rail</param>
+    /// <param name="momemtum">the ABSOLUTE velocity of the obj (calculated by hand)</param>
+    /// <returns>this functions return always in the same order of position : [Forward, Backward, actual, nearestPoint]</returns>
+    public Vector3[] _GetObjAllPos(float pos, float momentum)
+    {
+        Vector3[] results = new Vector3[4];
+
+        float forwardTarget = pos + momentum;
+        float backwardTarget = pos - momentum;
+
+        float totalDistance = 0f;
+
+        for (int i = 0; i < _railPointVec3.Length - 1; i++)
+        {
+            float segmentLength = Vector3.Distance(_railPointVec3[i], _railPointVec3[i + 1]);
+
+            // Forward
+            if (totalDistance + segmentLength >= forwardTarget)
+            {
+                float t = (forwardTarget - totalDistance) / segmentLength;
+                results[0] = Vector3.Lerp(_railPointVec3[i], _railPointVec3[i + 1], t);
+            }
+
+            // Backward
+            if (totalDistance + segmentLength >= backwardTarget)
+            {
+                float t = (backwardTarget - totalDistance) / segmentLength;
+                results[1] = Vector3.Lerp(_railPointVec3[i], _railPointVec3[i + 1], t);
+            }
+
+            // Actual
+            if (totalDistance + segmentLength >= pos)
+            {
+                float t = (pos - totalDistance) / segmentLength;
+                results[2] = Vector3.Lerp(_railPointVec3[i], _railPointVec3[i + 1], t);
+                results[3] = _railPointVec3[i]; // nearest previous point
+            }
+
+            totalDistance += segmentLength;
+        }
+
+        return results;
+    }
+
+
+
     private void OnValidate()
     {
         _lineRenderer = GetComponent<LineRenderer>();
--- Assets/Game/Scripts/Rails/RailObject.cs
+++ Assets/Game/Scripts/Rails/RailObject.cs
@@ -1,4 +1,4 @@
-using MoreMountains.Feedbacks;
+using System.Linq;
 using UnityEngine;
 
 public class RailObject : MonoBehaviour
@@ -8,33 +8,67 @@ public class RailObject : MonoBehaviour
     float _railLenght;
     [SerializeField] float _objRailPos = 0;
 
+    float _momemtum = 0f;
+    [SerializeField] float _defaultMomemtumAcceleration = 0.01f;
+    [SerializeField] float _maxMomemtum = .5f;
+    [SerializeField] float _treshHoldFurObjToFall = .1f;
+
+    [SerializeField,Range(0,1)] float LerpFactor = .1f;
     private void Start()
     {
+
         _railImOn._GetRailLenght();
         transform.position = _railImOn._GetObjPos(_objRailPos);
     }
-
+    GUIContent aa;
+    Vector3[] v3;
     private void Update()
     {
-        Vector3 forward = _railImOn._GetObjPos(_objRailPos + 0.1f);//All can be doone in one func for perf (forxward backward)
-        Vector3 backward = _railImOn._GetObjPos(_objRailPos - 0.1f);//All can be doone in one func for perf (forxward backward)
+        UpdatePhysics();
+    }
 
-        float difForward = forward.y - transform.position.y;
-        float difBackward = backward.y - transform.position.y;
+    private void UpdatePhysics()
+    {
+        //[Forward, Backward, actual, nearestPoint]
+        v3= _railImOn._GetObjAllPos(_objRailPos, _momemtum == 0 ? _defaultMomemtumAcceleration : _momemtum);
+        if (v3.All(v => v == Vector3.zero))
+        {
+            Debug.LogError("No valid positions returned from _GetObjAllPos. Skipping movement.");
+            return;
+        }
 
-        print(difForward);
+        float difForward = v3[0].y - transform.position.y;
+        float difBackward = v3[1].y - transform.position.y;
+
+        int i = 0;
+
         if (difForward < 0f)
         {
-            transform.position = forward;
-            _objRailPos += 0.1f;
-        }else if(difBackward < 0f)
+            _momemtum = Mathf.Min(_maxMomemtum, _maxMomemtum + _defaultMomemtumAcceleration);
+            _objRailPos += _momemtum;
+            transform.position = Vector3.Lerp(transform.position, v3[0], LerpFactor); // forward
+        }
+        else if (difBackward < 0f)
         {
-            transform.position = backward;
-            _objRailPos -= 0.1f;
+            _momemtum = Mathf.Min(_maxMomemtum, _maxMomemtum + _defaultMomemtumAcceleration);
+            _objRailPos -= _momemtum;
+            transform.position = Vector3.Lerp(transform.position, v3[1], LerpFactor); // Backward
         }
         else
         {
-            transform.position = _railImOn._GetObjPos(_objRailPos);
+            _momemtum = 0;
+            transform.position = Vector3.Lerp(transform.position, v3[3], .9f);
+        }
+
+        _objRailPos = Mathf.Clamp(_objRailPos, 0f, _railImOn._GetRailLenght());
+    }
+
+    private void OnGUI()
+    {
+        foreach (Vector3 v in v3)
+        {
+            aa = new GUIContent(v.ToString());
+            GUILayout.Label(aa);
         }
     }
 
