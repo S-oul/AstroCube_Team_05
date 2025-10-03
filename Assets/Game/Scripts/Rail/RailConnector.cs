7dc53dd5c50df6fb7d201b71e5c318badf56257a
Alienware110 <S.Soulslasher@gmail.com>
2025-10-03 12:03:52 +0200

    [WIP] Rail Re-import

---
+++ Assets/Game/Scripts/Rail/RailConnector.cs
@@ -0,0 +1,139 @@
+using NaughtyAttributes;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailConnector : MonoBehaviour
+{
+    [SerializeField] private Transform TestOBJ;
+
+    [SerializeField] private List<Transform> _railPoint = new List<Transform>();
+    [SerializeField] private Vector3[] _railPointVec3;
+
+    private RailObject _objOnRail;
+
+    public bool doRailLoop;
+
+    private float _railLenght =1;
+
+
+    private LineRenderer _lineRenderer;
+
+    private void Update()
+    {
+        _SetLineRendererPos();
+        _UpdateRailLenght(); // recalculate length
+    }
+
+    [Button("Update Line")]
+    void _SetLineRendererPos()
+    {
+        _railPointVec3 = new Vector3[_railPoint.Count];
+        int i = 0;
+        foreach (Transform t in _railPoint)
+        {
+            _railPointVec3[i++] = t.position;
+        }
+        _lineRenderer.positionCount = _railPointVec3.Length;
+        _lineRenderer.SetPositions(_railPointVec3);
+    }
+
+    public float _UpdateRailLenght()
+    {
+        float somme = 0;
+        for (int i = 0; i < _railPointVec3.Length - 1; i++)
+        {
+            somme += Vector3.Distance(_railPointVec3[i], _railPointVec3[i + 1]);
+        }
+
+        //update oj pos
+        if (_railLenght != somme)
+        {
+            //crossProduct
+            print("LENGHT IS DIFFERRENTE");
+            _objOnRail.ObjRailPos = (_objOnRail.ObjRailPos * somme) / _railLenght;
+            _objOnRail.UpdatePhysics();
+
+        }
+
+        return _railLenght = somme;
+    }
+
+    public RailObject ObjOnRail { get => _objOnRail; set => _objOnRail = value; }
+    public float RailLenght { get => _railLenght; set => _railLenght = value; }
+
+    public Vector3 _GetObjPos(float pos)
+    {
+        float sommeBefore = 0f;
+
+        for (int i = 0; i < _railPointVec3.Length - 1; i++)
+        {
+            float segLength = Vector3.Distance(_railPointVec3[i], _railPointVec3[i + 1]);
+
+            if (sommeBefore + segLength >= pos)
+            {
+                float localPos = (pos - sommeBefore) / segLength; // ratio mgl
+                return Vector3.Lerp(_railPointVec3[i], _railPointVec3[i + 1], localPos);
+            }
+
+            sommeBefore += segLength;
+        }
+
+        return _railPointVec3[_railPointVec3.Length - 1];
+    }
+
+    public struct RailInfo
+    {
+        public Vector3 position;
+        public Vector3 direction;    //slope dir // dir.y is the slope angle downward 
+        public Vector3 nearestPoint; // Nearest previous rail point | this maybe usefull in an edghecase situation;
+    }
+
+    /// <summary>
+    /// This function gives no fuck about you
+    /// </summary>
+    /// <param name="railPos">The position on the rail '_objRailPos'</param>
+    /// <returns>a RailInfo with in it the new position, slope direction, and the NearestPoint (anchor of therail)</returns>
+    public RailInfo _GetRailInfoAtPos(float railPos)
+    {
+        float totalDistance = 0f;
+
+        for (int i = 0; i < _railPointVec3.Length - 1; i++)
+        {
+            Vector3 start = _railPointVec3[i];
+            Vector3 end = _railPointVec3[i + 1];
+            float segmentLength = Vector3.Distance(start, end);
+
+            if (totalDistance + segmentLength >= railPos)
+            {
+                float t = (railPos - totalDistance) / segmentLength;
+                Vector3 position = Vector3.Lerp(start, end, t);
+                Vector3 direction = (end - start).normalized;
+
+                return new RailInfo
+                {
+                    position = position,
+                    direction = direction,
+                    nearestPoint = start
+                };
+            }
+
+            totalDistance += segmentLength;
+        }
+
+        // if rail obj is at the end of the rail;
+        return new RailInfo
+        {
+            position = _railPointVec3[_railPointVec3.Length - 1],
+            direction = Vector3.zero,
+            nearestPoint = _railPointVec3[_railPointVec3.Length - 2]
+        };
+    }
+
+
+    private void OnValidate()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _SetLineRendererPos();
+    }
+
+}
---
+++ Assets/Game/Scripts/Rail/RailObject.cs
@@ -0,0 +1,76 @@
+using NaughtyAttributes;
+using System.Linq;
+using UnityEngine;
+using static RailConnector;
+
+public class RailObject : MonoBehaviour
+{
+    [SerializeField] RailConnector _railImOn;
+
+    float _railLenght;
+    [SerializeField] float _objRailPos = 0;
+
+    [ShowNonSerializedField] float _momemtum = 0f;
+    [SerializeField] float _maxMomemtum = .5f;
+
+    [SerializeField] float _gravityFactor = 1;
+
+    [SerializeField, Range(0, 1), InfoBox("If 0 or 1 : it will auto snap to the Newpos")] float _lerpFactor = .1f;
+
+
+    bool UpdatedThisFrame = false;
+    private void Start()
+    {
+        _railImOn.ObjOnRail = this;
+        transform.position = _railImOn._GetObjPos(ObjRailPos);
+    }
+
+    #region acccesseur
+    public float ObjRailPos { get => _objRailPos; set => _objRailPos = value; }
+
+    #endregion
+
+    private void LateUpdate()
+    {
+        UpdatePhysics();
+        UpdatedThisFrame = false;
+    }
+
+    public void UpdatePhysics()
+    {
+        if (UpdatedThisFrame) return;
+        UpdatedThisFrame = true;
+
+        RailInfo info = _railImOn._GetRailInfoAtPos(ObjRailPos);
+        print("DOT : " + Vector3.Dot(info.position - transform.position, info.direction) + "Acceleration : " + Physics.gravity.y * info.direction.y * _gravityFactor);
+        if (Vector3.Dot(info.position - transform.position,info.direction) > 0)
+        {
+            _momemtum = 0f;
+        }
+        else
+        {
+            float acceleration = Physics.gravity.y * info.direction.y * _gravityFactor;
+            _momemtum += acceleration * Time.deltaTime;
+            //print(_momemtum + " // dir : " + info.direction.y);
+        }
+
+
+
+        _momemtum = Mathf.Clamp(_momemtum, -_maxMomemtum, _maxMomemtum);
+
+        ObjRailPos += _momemtum * Time.deltaTime;
+        ObjRailPos = Mathf.Clamp(ObjRailPos, 0f, _railImOn.RailLenght);
+
+        transform.position = (_lerpFactor == 0 || _lerpFactor == 1) ? info.position : Vector3.Lerp(transform.position, info.position, _lerpFactor);
+
+        /*if (Mathf.Abs(_momemtum) < 0.1f)
+        {
+            _momemtum = 0f;
+        }*/
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label(_momemtum.ToString());
+    }
+}
