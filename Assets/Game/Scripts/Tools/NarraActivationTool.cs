925d1a95ca3dc6d452ef473df8b39c0eaa30883d
ShaneDorin <shane.dorin@edu.devinci.fr>
2025-10-15 17:00:53 +0200

    [ADD] warning message to narra deactivation

--- Assets/Game/Scripts/Tools/NarraActivationTool.cs
+++ Assets/Game/Scripts/Tools/NarraActivationTool.cs
@@ -1,13 +1,25 @@
 using MoreMountains.FeedbacksForThirdParty;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NarraActivationTool : MonoBehaviour
 {
     [SerializeField] bool _isNarraActiveTool = true;
 
-
     public bool IsNarraActiveTool
     {
+#if UNITY_EDITOR
         get => _isNarraActiveTool;
+#else
+        get => true;
+#endif
+    }
+
+    private void Start()
+    {
+        if (_isNarraActiveTool == false)
+        {
+            Debug.Log("WARNING ! Narrative scene triggers are currently DEACTIVATED in this scene.");
+        }
     }
 }
