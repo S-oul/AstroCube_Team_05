3bb4ff2ce6b9625895708bb78f9dacc2174ef467
Nic0InTheRadio <112950466+Nic0InTheRadio@users.noreply.github.com>
2025-10-28 16:12:10 +0100

    [UPDATE] FEEL UPDATE + SCREEN SHAKE FIX

--- Assets/Feel/MMFeedbacks/MMFeedbacksForThirdParty/NiceVibrations/Feedbacks/MMF_NVEmphasis.cs
+++ Assets/Feel/MMFeedbacks/MMFeedbacksForThirdParty/NiceVibrations/Feedbacks/MMF_NVEmphasis.cs
@@ -3,6 +3,7 @@ using MoreMountains.Feedbacks;
 #if MOREMOUNTAINS_NICEVIBRATIONS_INSTALLED
 using Lofelt.NiceVibrations;
 #endif
+using UnityEngine.Scripting.APIUpdating;
 
 namespace MoreMountains.FeedbacksForThirdParty
 {
@@ -13,6 +14,7 @@ namespace MoreMountains.FeedbacksForThirdParty
 	#if MOREMOUNTAINS_NICEVIBRATIONS_INSTALLED
 	[FeedbackPath("Haptics/Haptic Emphasis")]
 	#endif 
+	[MovedFrom(false, null, "MoreMountains.Feedbacks.NiceVibrations")]
 	[FeedbackHelp("Use this feedback to play an Emphasis haptics, short haptic bursts whose amplitude and frequency can be controlled in real time, also called Transients in CoreHaptics/iOS")]
 	public class MMF_NVEmphasis : MMF_Feedback
 	{
@@ -20,6 +22,7 @@ namespace MoreMountains.FeedbacksForThirdParty
 		/// a static bool used to disable all feedbacks of this type at once
 		public static bool FeedbackTypeAuthorized = true;
 		#if UNITY_EDITOR
+		public override bool HasCustomInspectors => true;
 		public override Color FeedbackColor { get { return MMFeedbacksInspectorColors.HapticsColor; } }
 		#endif
         
@@ -44,9 +47,21 @@ namespace MoreMountains.FeedbacksForThirdParty
 		public float MaxFrequency = 1f;
 
 		[MMFInspectorGroup("Settings", true, 16)]
+		/// a debug button that lets you test the haptic file from its inspector
+		public MMF_Button PlayEmphasisButton;
+		
 		/// a set of settings you can tweak to specify how and when exactly this haptic should play
 		[Tooltip("a set of settings you can tweak to specify how and when exactly this haptic should play")]
 		public MMFeedbackNVSettings HapticSettings;
+		
+		/// <summary>
+		/// Initializes custom buttons
+		/// </summary>
+		public override void InitializeCustomAttributes()
+		{
+			base.InitializeCustomAttributes();
+			PlayEmphasisButton = new MMF_Button("Test Emphasis", PlayEmphasis);
+		}
         
 		/// <summary>
 		/// On play, we randomize our amplitude and frequency and play our emphasis haptic
@@ -55,16 +70,25 @@ namespace MoreMountains.FeedbacksForThirdParty
 		/// <param name="feedbacksIntensity"></param>
 		protected override void CustomPlayFeedback(Vector3 position, float feedbacksIntensity = 1.0f)
 		{
-			if (!Active || !FeedbackTypeAuthorized || !HapticSettings.CanPlay())
+			if (!Active || !FeedbackTypeAuthorized || HapticSettings == null || !HapticSettings.CanPlay())
 			{
 				return;
 			}
 
+			PlayEmphasis();
+		}
+
+		/// <summary>
+		/// Plays the specified emphasis haptic
+		/// </summary>
+		protected virtual void PlayEmphasis()
+		{
 			float amplitude = Random.Range(MinAmplitude, MaxAmplitude);
 			float frequency = Random.Range(MinFrequency, MaxFrequency);
 			HapticSettings.SetGamepad();
 			HapticPatterns.PlayEmphasis(amplitude, frequency);
 		}
+		
 		#else
 		protected override void CustomPlayFeedback(Vector3 position, float feedbacksIntensity = 1.0f) { }
 		#endif
