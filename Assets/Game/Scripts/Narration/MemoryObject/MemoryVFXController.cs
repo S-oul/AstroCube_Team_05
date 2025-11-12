a628e7bc65feac4b9031a25c8046df191197ebea
BBouleDeSuiff <emaouardi@gmail.com>
2025-11-12 15:03:22 +0100

    [ADD] Fonctionnal Memory Object morph to LD Element

--- Assets/Game/Scripts/Narration/MemoryObject/MemoryVFXController.cs
+++ Assets/Game/Scripts/Narration/MemoryObject/MemoryVFXController.cs
@@ -5,7 +5,17 @@ using UnityEngine.VFX;
 public class MemoryVFXController : MonoBehaviour
 {
     [SerializeField] private VisualEffect _vfx;
+    [SerializeField] private float _stayDuration;
+    [SerializeField] private bool _spawnsLDElement;
+    [SerializeField, ShowIf("_spawnsLDElement")] private GameObject _LDElement;
     private Transform _origin;
+
+    /*
+     Lerp_Delta:
+    0 = At Origin
+    0.5 = At character   STAY DURING _stayDuration
+    1 : At LD Elements     STAY INDEFINITELY
+    */
 
     void Start()
     {
@@ -23,7 +33,17 @@ public class MemoryVFXController : MonoBehaviour
     {
         _origin = transform.Find("Origin - VFX");
         _vfx.SetVector3("Origin", _origin.transform.localPosition);
+        if (_LDElement)
+        {
+            _vfx.SetVector3("Origin_LD_Element_Position", _LDElement.transform.localPosition);
+            _vfx.SetVector3("Origin_LD_Element_Rotation", _LDElement.transform.rotation.eulerAngles);
+            _vfx.SetVector3("Origin_LD_Element_Scale", _LDElement.transform.localScale);
+        }
+        //mettre l'objet en enfant pour avoir la bonne position (local ?)
+        // desactiver le mesh renderer et activer le collider après toute l'animation
+        //Il faut que les models 3D soient READABLE
     }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
