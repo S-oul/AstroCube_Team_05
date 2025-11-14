dd8b231101939ef3861f37461186ed5159db4f20
ShaneDorin <shane.dorin@edu.devinci.fr>
2025-11-14 11:36:49 +0100

    [ADD] working restart controls pop-up system
    triggered by player doing 5 moves.

--- Assets/Game/Scripts/UI/ActivateControlTips.cs
+++ Assets/Game/Scripts/UI/ActivateControlTips.cs
@@ -1,4 +1,7 @@
+using System;
 using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.Events;
 
 public class ActivateControlTips : MonoBehaviour
 {
@@ -6,27 +9,54 @@ public class ActivateControlTips : MonoBehaviour
     [SerializeField] GameObject _restartControlTipPopUp;
     [SerializeField] int _numOfRotationsBeforPopUp;
 
+    // triggers with 'true' when the suggestion is activated (made visible) and 'false' when the suggestion is diactivated (made invisible). 
+    [SerializeField] UnityEvent<bool> SuggestRestartEvent;
+    static event Action<bool> OnSuggestRestart; 
+
+    private void Start()
+    {
+
+        if (_restartControlTipPopUp == null)
+        {
+            Debug.Log("No ControlTip GameObject assigned to ActivateControlTips script.");
+            return;
+        }
+        if (_restartControlTipPopUp.activeSelf) _restartControlTipPopUp.SetActive(false);
+    }
+
     private void OnEnable()
     {
         EventManager.OnEndCubeRotation += Add1ToRotationCount;
         EventManager.OnPlayerReset += ResetRotationCound;
+        OnSuggestRestart += SuggestRestartEvent.Invoke;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnEndCubeRotation -= Add1ToRotationCount;
+        EventManager.OnPlayerReset -= ResetRotationCound;
+        OnSuggestRestart -= SuggestRestartEvent.Invoke;
     }
 
     void Add1ToRotationCount()
     {
-        _rotationCount++;
+        if (_restartControlTipPopUp == null) return;
 
-        if (_rotationCount >= _numOfRotationsBeforPopUp &&
-            _restartControlTipPopUp.activeSelf == false) 
-            _restartControlTipPopUp.SetActive( true );
+        _rotationCount++;
+        if (_rotationCount >= _numOfRotationsBeforPopUp) OnSuggestRestart.Invoke(true);
     }
 
-    void ResetRotationCound()
+    void ResetRotationCound(float iDontNeedThisFloat)
     {
-        _rotationCount = 0;
-        if (_restartControlTipPopUp.activeSelf)
-        {
-            _restartControlTipPopUp.SetActive ( false );
-        }
+        if (_restartControlTipPopUp == null) return;
+
+        _rotationCount *= -1;
+        OnSuggestRestart.Invoke(false);
+    }
+
+    public void SetPlaceHolderPopUpActive(bool newActiveStatus)
+    {
+        if (_restartControlTipPopUp.activeSelf == !newActiveStatus)
+            _restartControlTipPopUp.SetActive(newActiveStatus);
     }
 }
