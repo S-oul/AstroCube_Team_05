0d81615916ad23a3d814c9c6c7be99260ec09fcd
ShaneDorin <shane.dorin@edu.devinci.fr>
2025-11-25 11:45:21 +0100

    [FIX] added clamp to artCube smooth damping so that it always stays on screen.

--- Assets/Game/Scripts/Gamefeel/SmoothDamping.cs
+++ Assets/Game/Scripts/Gamefeel/SmoothDamping.cs
@@ -6,6 +6,8 @@ public class SmoothDamping : MonoBehaviour
     [SerializeField] Transform _playerTransform;
     [SerializeField] float _verticalSmoothAmount;
     [SerializeField] float _intencity;
+    [SerializeField] float _maxHight;
+    [SerializeField] float _minHight;
 
     Vector3 _artCubeStartLocalPos;
     float _imaginaryCubePos;
@@ -19,6 +21,7 @@ public class SmoothDamping : MonoBehaviour
 
     private void LateUpdate()
     {
+        // calculate _verticalVelocity
         float newImaginaryCubePos = Mathf.SmoothDamp(
             _imaginaryCubePos,
             _playerTransform.position.y,
@@ -27,6 +30,14 @@ public class SmoothDamping : MonoBehaviour
         );
         _imaginaryCubePos = newImaginaryCubePos;
 
-        transform.localPosition = _artCubeStartLocalPos - Vector3.up * _verticalVelocity * _intencity;
+        // clamp
+        if (_imaginaryCubePos - _playerTransform.position.y > _maxHight) _imaginaryCubePos = _playerTransform.position.y + _maxHight;
+        if (_imaginaryCubePos - _playerTransform.position.y < _minHight) _imaginaryCubePos = _playerTransform.position.y + _minHight;
+
+        // create new pos
+        transform.localPosition = 
+            _artCubeStartLocalPos - Vector3.up *
+            _verticalVelocity *
+            _intencity;
     }
 }
