9fa999c0c0720ade039e91d8111b603ffe3f0032
Alienware110 <Paul.demosthene2@gmail.com>
2025-11-26 10:17:57 +0100

    [FIX] Remove Arrow in playMod

--- Assets/Game/Scripts/LD_Elements/ChangeReset.cs
+++ Assets/Game/Scripts/LD_Elements/ChangeReset.cs
@@ -4,5 +4,13 @@ public class ChangeReset : MonoBehaviour
 {
     [SerializeField] Transform _newResetPos;
 
+    [SerializeField] GameObject _arrow;
+
+
     public Transform NewResetPos { get => _newResetPos; set => _newResetPos = value; }
+
+    private void Start()
+    {
+        _arrow.SetActive(false);
+    }
 }
