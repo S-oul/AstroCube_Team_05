7814940cf3e3042a01bbffe1ec5352cfe59d6362
leopoldlecl <115979417+LeopoldLecl@users.noreply.github.com>
2025-11-27 17:31:47 +0100

    [ADD] UI prefab to dev safely

--- Assets/Game/Scripts/Player/PlayerTrigger.cs
+++ Assets/Game/Scripts/Player/PlayerTrigger.cs
@@ -75,7 +75,6 @@ public class PlayerTrigger : MonoBehaviour
                 EventManager.TriggerLevelFinished();
 
                 if (!_victoryZoneEvent.IsNull) RuntimeManager.PlayOneShot(_victoryZoneEvent);
-                EventManager.TriggerPlayerWin();
                 Destroy(other.gameObject);
                 break;
 
