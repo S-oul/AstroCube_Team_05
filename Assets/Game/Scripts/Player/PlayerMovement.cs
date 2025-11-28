157d1f768dc0f654d5ee7b34505dd4a032cbc0b3
ShaneDorin <shane.dorin@edu.devinci.fr>
2025-11-28 11:02:41 +0100

    [DEBUG] changed the vertical clamp parameters on the Art Cube
    Also set a maximum fall speed for the player.

--- Assets/Game/Scripts/Player/PlayerMovement.cs
+++ Assets/Game/Scripts/Player/PlayerMovement.cs
@@ -44,6 +44,7 @@ public class PlayerMovement : MonoBehaviour
     float _currentMoveSpeed;
     float _currentMoveSpeedFactor = 1f;
     Vector3 _verticalVelocity;
+    float _currentFallSpeed;
     Vector3 _horizontalVelocity;
     bool _isGrounded;
 
@@ -129,10 +130,20 @@ public class PlayerMovement : MonoBehaviour
         if (_hasGravity)
         {
             _gravityDirection = transform.up;
-            _verticalVelocity += _gravityDirection * (Math.Clamp(_gameSettings.Gravity * Time.deltaTime, 0, _maxPlayerFallSpeed));
 
-            if (_isGrounded && _verticalVelocity.y <= 0)
+            //_verticalVelocity += _gravityDirection * (Math.Clamp(_gameSettings.Gravity * Time.deltaTime, 0, _maxPlayerFallSpeed));
+
+            _currentFallSpeed += _gameSettings.Gravity * Time.deltaTime; // this is not used directly but helps track the current vertical velocity. 
+            if (_currentFallSpeed > _maxPlayerFallSpeed * -1) // only add to the vertical velocity if fall speed is above the minimum vertical velocity. 
             {
+                _verticalVelocity += _gravityDirection * (_gameSettings.Gravity * Time.deltaTime);
+            }
+
+            Debug.Log(_currentFallSpeed);
+
+            if (_isGrounded && _currentFallSpeed <= 0)
+            {
+                _currentFallSpeed = 0;
                 _verticalVelocity = Vector3.zero;
             }
         }
