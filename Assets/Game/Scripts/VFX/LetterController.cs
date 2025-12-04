d57ed0dfd37a36947afe2b8b1d0f75c6934b792d
Kévin Kaito_OwO Verhaeghe <advestankv@gmail.com>
2025-12-04 23:25:09 +0100

    ADD subtitles

--- Assets/Game/Scripts/VFX/LetterController.cs
+++ Assets/Game/Scripts/VFX/LetterController.cs
@@ -7,12 +7,10 @@ public class LetterController : MonoBehaviour
 {
     [SerializeField] private float _fxSlider = 0.0f;
     [SerializeField] private Image _letter;
-    [SerializeField] private StudioEventEmitter _emitter;
     private Material _mat;
 
     void Start()
     {
-        _emitter.enabled = true;
         _mat = _letter.material;
     }
 
