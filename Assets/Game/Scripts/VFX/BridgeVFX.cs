802f6928300b70ff1d598055347117fbc28f58dc
CamilleS <cs.sabycamille@gmail.com>
2025-12-10 21:16:48 +0100

    [AUDIO] (SFX) Bridge Level10

--- Assets/Game/Scripts/VFX/BridgeVFX.cs
+++ Assets/Game/Scripts/VFX/BridgeVFX.cs
@@ -1,13 +1,17 @@
-using FMOD;
 using System;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.VFX;
+using FMODUnity;
 
 public class BridgeVFX : MonoBehaviour
 {
+    [SerializeField] private EventReference _BridgeSound;
+
+    private FMOD.Studio.EventInstance _audioInstance;
     private Transform _player;
     private VisualEffect _vfx;
+    
 
     public Bounds BoxExtent = new Bounds();
 
@@ -16,43 +20,61 @@ public class BridgeVFX : MonoBehaviour
         BoxExtent.center = transform.position;
         _player = GameObject.FindGameObjectWithTag("Player").transform;
         _vfx = GetComponent<VisualEffect>();
+        
+        _audioInstance = RuntimeManager.CreateInstance(_BridgeSound);
     }
 
-    float oldDist;
+    private Vector3 _lastPlayerPos;
+    private float _stopTimer;
+    [SerializeField] private float _stopDelay = 0.1f;
+    [SerializeField] private float _maxDistanceUpdate = 50f;
     public bool ShouldPlaySound = false;
 
     void Update()
     {
-        //Calculate The Distance then set it ?
         _vfx.SetVector3("PlayerPos", _player.localPosition);
 
+        float moveDist = Vector3.Distance(_player.position, _lastPlayerPos);
+        bool isMoving = moveDist > 0.001f;
+        bool isInside = BoxExtent.Contains(_player.position);
 
-        var newDist = Vector3.Distance(_player.position, transform.position);
-        if (Mathf.Abs(newDist - oldDist) > float.Epsilon && BoxExtent.Contains(_player.position))
+        if (isInside && isMoving)
         {
+            _stopTimer = 0f;
+
             if(!ShouldPlaySound)
             {
                 ShouldPlaySound = true;
-                print("CACA PLAY");
-                //PLAY FMOD HERE;
+                _audioInstance.start();
             }
         }
         else
         {
-            if (ShouldPlaySound)
+            _stopTimer += Time.deltaTime;
+
+            if (ShouldPlaySound && _stopTimer > _stopDelay)
             {
-                print("CACA STOP");
-
                 ShouldPlaySound = false;
-                //stop FMOD HERE;
+                _audioInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
             }
-
         }
 
+        if (BoxExtent.SqrDistance(_player.position) < (_maxDistanceUpdate * _maxDistanceUpdate))
+        {
+            Vector3 targetPos = BoxExtent.ClosestPoint(_player.position);
+            _audioInstance.set3DAttributes(RuntimeUtils.To3DAttributes(targetPos));
+        }
 
+        _lastPlayerPos = _player.position;
+    }
 
-
-        oldDist = newDist;
+    private void OnDestroy()
+    {
+        if (_audioInstance.isValid())
+        {
+            _audioInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            _audioInstance.release();
+        }
     }
 
     private void OnDrawGizmos()
