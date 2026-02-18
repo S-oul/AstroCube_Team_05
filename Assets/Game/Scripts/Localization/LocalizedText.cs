eb65cfa2e04247438bdf4fd6e3b4ca180d096c04
Kévin Kaito_OwO Verhaeghe <advestankv@gmail.com>
2026-02-18 04:41:24 +0100

    ADD lil tool for GD Localization

--- Assets/Game/Scripts/Localization/LocalizedText.cs
+++ Assets/Game/Scripts/Localization/LocalizedText.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEditor;
 
 [RequireComponent(typeof(TMP_Text))]
 public class LocalizedText : MonoBehaviour
@@ -8,6 +10,9 @@ public class LocalizedText : MonoBehaviour
 
     [SerializeField] private string _csvName;
     [SerializeField] private string _localizationId;
+
+    public string CSVName => _csvName;
+    public string LocalizationId => _localizationId;
     
     private TMP_Text _text;
 
@@ -19,5 +24,37 @@ public class LocalizedText : MonoBehaviour
     private void Start()
     {
         _text.text = LocalizationManager.Instance.GetString(_csvName, _localizationId);
+    }
+}
+
+[CustomEditor(typeof(LocalizedText))]
+public class LocalizedTextCustomEditor : Editor
+{
+    private static string _currentText;
+    private static Dictionary<(string csv, string id, ELanguage language), string> _texts;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        LocalizedText localizedText = (LocalizedText)target;
+        _texts = LocalizationManager.GenerateCSVInEditor();
+        GUILayout.Space(20);
+        
+        if (_texts.TryGetValue((localizedText.CSVName, localizedText.LocalizationId, ELanguage.ENGLISH), out var text))
+        {
+            _currentText = text;
+            GUI.color = Color.green;
+            GUILayout.Label("Text Found", new GUIStyle(GUI.skin.label){fontStyle = FontStyle.Bold, fontSize = 15});
+            GUI.color = Color.white;
+            GUILayout.Label(_currentText);
+        }
+        else
+        {
+            GUI.color = Color.red;
+            GUILayout.Label("Error", new GUIStyle(GUI.skin.label){fontStyle = FontStyle.Bold, fontSize = 15});
+            GUI.color = Color.white;
+            GUILayout.Label($"No text found for <{localizedText.CSVName}:{localizedText.LocalizationId}>");
+        }
     }
 }
