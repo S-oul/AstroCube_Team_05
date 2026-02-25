de842eb006ce387ef9ba08be3608ca0c5cad9248
Kévin Kaito_OwO Verhaeghe <advestankv@gmail.com>
2026-02-25 17:08:20 +0100

    ADD tool

--- Assets/Game/Scripts/GameActions/Actions/GameActionShowSubtitle.cs
+++ Assets/Game/Scripts/GameActions/Actions/GameActionShowSubtitle.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using System.Collections.Generic;
 using FMODUnity;
+using UnityEditor;
 using UnityEngine;
 
 public class GameActionShowSubtitle : AGameAction
@@ -9,6 +11,9 @@ public class GameActionShowSubtitle : AGameAction
     [SerializeField] private string _localizationID;
     [SerializeField] private float _duration;
     [SerializeField] private Color _color;
+    
+    public string CSVName => _csvName;
+    public string LocalizationID => _localizationID;
 
     private bool _isFinished = true;
     
@@ -38,4 +43,36 @@ public class GameActionShowSubtitle : AGameAction
         return _isFinished;
     }
     
+}
+
+[CustomEditor(typeof(GameActionShowSubtitle))]
+public class ShowSubtitleCustomEditor : Editor
+{
+    private static string _currentText;
+    private static Dictionary<(string csv, string id, ELanguage language), string> _texts;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        GameActionShowSubtitle subtitle = (GameActionShowSubtitle)target;
+        _texts = LocalizationManager.GenerateCSVInEditor();
+        GUILayout.Space(20);
+        
+        if (_texts.TryGetValue((subtitle.CSVName, subtitle.LocalizationID, ELanguage.ENGLISH), out var text))
+        {
+            _currentText = text;
+            GUI.color = Color.green;
+            GUILayout.Label("Text Found", new GUIStyle(GUI.skin.label){fontStyle = FontStyle.Bold, fontSize = 15});
+            GUI.color = Color.white;
+            GUILayout.Label(_currentText);
+        }
+        else
+        {
+            GUI.color = Color.red;
+            GUILayout.Label("Error", new GUIStyle(GUI.skin.label){fontStyle = FontStyle.Bold, fontSize = 15});
+            GUI.color = Color.white;
+            GUILayout.Label($"No text found for <{subtitle.CSVName}:{subtitle.LocalizationID}>");
+        }
+    }
 }
